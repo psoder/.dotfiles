22dec3b9c4866ece53a72a57ed8f5c7c06b27ebd
Pontus <52171526+psoder@users.noreply.github.com>
2022-02-22 23:16:21 +0100

    More VS Code config

--- apps/vscode-extensions.sh
+++ apps/vscode-extensions.sh
@@ -2,48 +2,31 @@
 
 # Install VS Code extensions
 extensions=(
-  CoenraadS.bracket-pair-colorizer
+  # Utilities
   DavidAnson.vscode-markdownlint
+  VisualStudioExptTeam.vscodeintellicode
+  aeschli.vscode-css-formatter
+  darkriszty.markdown-table-prettify
   dbaeumer.vscode-eslint
   dsznajder.es7-react-js-snippets
   ecmel.vscode-html-css
   esbenp.prettier-vscode
   formulahendry.auto-close-tag
   formulahendry.auto-rename-tag
+  formulahendry.code-runner
+  goessner.mdmath
   ms-azuretools.vscode-docker
   oderwat.indent-rainbow
   redhat.vscode-yaml
+  henriiik.vscode-sort
   VisualStudioExptTeam.vscodeintellicode
-  aeschli.vscode-css-formatter
-  # bmewburn.vscode-intelephense-client
-  # darkriszty.markdown-table-prettify
-  # esbenp.prettier-vscode
-  # formulahendry.auto-close-tag
-  # formulahendry.auto-rename-tag
-  # formulahendry.code-runner
-  # goessner.mdmath
-  # henriiik.vscode-sort
-  # ing-bank.vscode-psl
-  # jchannon.csharpextensions
-  # lucas-rezende.greek-alphabet-snippets
-  # mgmcdermott.vscode-language-babel
-  # ms-azuretools.vscode-docker
-  # ms-dotnettools.csharp
-  # ms-vscode.vscode-typescript-next
-  # naco-siren.gradle-language
-  # Pivotal.vscode-spring-boot
-  # redhat.java
-  # Tobiah.unity-tools
-  # Unity.unity-debug
-  # VisualStudioExptTeam.vscodeintellicode
-  # vscjava.vscode-java-debug
-  # vscjava.vscode-java-dependency
-  # vscjava.vscode-java-pack
-  # vscjava.vscode-java-test
-  # xabikos.JavaScriptSnippets
-  # xdebug.php-debug
-  # xdebug.php-pack
-  # zobo.php-intellisense
+  lucas-rezende.greek-alphabet-snippets
+  ms-azuretools.vscode-docker
+
+  # Themes
+  eliverlara.andromeda
+  sdras.night-owl
+  tinkertrain.theme-panda
 )
 
 for i in ${extensions[@]}; do
