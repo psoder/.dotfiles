6c4c9efe83659f630c2a497155226b92a53ed10e
Pontus Söderlund <pontus@clira.io>
2024-02-03 15:09:10 +0100

    Update keybinds

--- nvim/nvchad.custom/configs/mappings.lua
+++ nvim/nvchad.custom/configs/mappings.lua
@@ -9,11 +9,19 @@ M.disabled = {
 
 M.abc = {
   n = {
-    ["<C-d>"] = {"<C-d>zz", "Half window up and center"},
-    ["<C-u>"] = {"<C-u>zz", "Half window down and center"},
+    ["<C-d>"] = { "<C-d>zz", "Half window up and center" },
+    ["<C-u>"] = { "<C-u>zz", "Half window down and center" },
+    ["n"] = { "nzzzv", "Keep search term in middle" },
+    ["N"] = { "Nzzzv", "Keep search term in middle" },
+    ["<leader>s"] = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]] }
   },
 
-  i = {},  
+  i = {},
+
+  v = {
+    ["K"] = { ":m '<-2<CR>gv=gv", "Move line up" },
+    ["J"] = { ":m '>+1<CR>gv=gv", "Move line down" },
+  },
 }
 
 return M
