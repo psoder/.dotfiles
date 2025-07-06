f0c90561fedcfe07d765f9203a1482dc5e2c3a3e
Pontus Söderlund <pontus@clira.io>
2025-07-06 22:35:09 +0200

    Install Obsidian

--- nix/hosts/orion/home.nix
+++ nix/hosts/orion/home.nix
@@ -31,6 +31,7 @@
     bun
 
     spotify
+    obsidian
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
