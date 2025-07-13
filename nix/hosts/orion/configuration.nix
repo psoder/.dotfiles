2ed30ff6b95eb2ca84b35489bcc536cae08d7d94
Pontus Söderlund <pontus@clira.io>
2025-07-13 11:34:29 +0200

    feat: use latest kernel

--- nix/hosts/orion/configuration.nix
+++ nix/hosts/orion/configuration.nix
@@ -14,6 +14,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "orion"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
