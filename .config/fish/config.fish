ce8c706eb45d1ebe6ddb8386e913ce57637f59fb
Pontus Söderlund <pontus@clira.io>
2025-09-13 22:57:53 +0200

    feat: enable fish vi mode

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,5 +21,17 @@ if status is-interactive
 	else
 		zellij
 	end
+
+	function fish_user_key_bindings
+	    # Execute this once per mode that emacs bindings should be used in
+	    fish_default_key_bindings -M insert
+
+	    # Then execute the vi-bindings so they take precedence when there's a conflict.
+	    # Without --no-erase fish_vi_key_bindings will default to
+	    # resetting all bindings.
+	    # The argument specifies the initial mode (insert, "default" or visual).
+	    fish_vi_key_bindings --no-erase insert
+	end
+
 end
 
